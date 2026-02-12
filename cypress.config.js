0d1f8c625105368cd27330c338d0005d2603e498
Matheus-sp12 <viniciussilva_1@hotmail.com>
2026-02-12 15:40:09 -0300

    primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: 'https://restful-booker.herokuapp.com',
+    requestTimeout: 6000,
+    responseTimeout: 6000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env:{
+      requestMode: true,
+      hidenCredentials: true,
+    }
+  },
+});
