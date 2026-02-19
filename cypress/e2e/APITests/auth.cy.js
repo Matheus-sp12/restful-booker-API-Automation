8057aa36b52f9392d38496abde5cc5641a07a987
Matheus-sp12 <viniciussilva_1@hotmail.com>
2026-02-19 16:37:51 -0300

    add response status

--- cypress/e2e/APITests/auth.cy.js
+++ cypress/e2e/APITests/auth.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Test Suit - Auth API Testing", () => {
+  it("1 - POST credentials to aut endpoint with sucess", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth",
+      body: {
+        username: "admin",
+        password: "password123",
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("token");
+      expect(response.body).not.to.be.empty;
+    });
+  });
+});
